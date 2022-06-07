4b4a391637be822f929664434f45ec910e544eac
areskuelap <83573119+areskuelap@users.noreply.github.com>
2022-06-07 14:57:29 -0500

    Update PaginationButtons.js

--- components/PaginationButtons.js
+++ components/PaginationButtons.js
@@ -6,11 +6,13 @@ const PaginationButtons = () => {
   const router = useRouter();
 
   const startIndex = Number(router.query.start) || 0;
+  const langName = String(router.query.lan) || 'en';
   return (
     <div className="flex max-w-lg justify-between text-blue-700 mb-10">
       {startIndex >= 10 && (
         <Link
-          href={`/search?term=${router.query.term}&start=${startIndex - 10}`}
+          // href={`/search?term=${router.query.term}&start=${startIndex - 10}&lan=${router.query.lan||'en'}`}
+          href={`/search?term=${router.query.term}&start=${startIndex - 10}&lan=${langName}`}
         >
           <div className="flex flex-grow flex-col items-center cursor-pointer hover:underline">
             <ChevronLeftIcon className="h-5" />
@@ -18,7 +20,7 @@ const PaginationButtons = () => {
           </div>
         </Link>
       )}
-      <Link href={`/search?term=${router.query.term}&start=${startIndex + 10}`}>
+      <Link href={`/search?term=${router.query.term}&start=${startIndex + 10}&lan=${router.query.lan||'en'}`}>
         <div className="flex flex-grow flex-col items-center cursor-pointer hover:underline">
           <ChevronRightIcon className="h-5" />
           <p>Next</p>
