0b57fc31f34309bfe76bbac72c91136fcdfa4ed3
areskuelap <83573119+areskuelap@users.noreply.github.com>
2022-06-13 23:27:40 -0500

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -18,21 +18,22 @@ export default function About(data) {
 
       <HeaderPage />
 
-      <div className="page_conentst mx-auto w-full px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
-        <div className="page_conentst_title">
-          <strong><h1>About Us</h1></strong>
-
+      <div style="padding-top: 80px; padding-left: 170px; color: #2ca7eec2"> 
+        <h2>About us</h2>
+    </div>
+      <div style="font-family: verdana; padding-top: 2px; padding-left: 170px">
+        <p style="color: #2ca7eec2"> <strong>Ownnership</strong> </p>
+        <p>Konlap Web Browser and this website are owned and operated by Konlap Technologies E.I.R.L. <br>We are an enterprise based on Amazonas (Peru) and
+            our goal is to reduce the Internet's language <br> barrier by allowing people to find and access information in 100+ languages. 
+        <br>Keep updated about us <a href="https://www.linkedin.com/company/konlap" target="_blank">here</a>.</p>
         </div>
-        <div className="page_conentst_desc">
-          
-            
-            <p><strong>Ownership and Contact</strong><br></br> Konlap Web Browser is owned and operated by Konlap Technologies E.I.R.L. a Peruvian company, directed and managed from its headquarters in Amazonas (Peru). You can contact us at konlaptechs@gmail.com</p>
-            </div><div>
+        <div style="font-family: verdana; padding-top: 12px; padding-left: 170px">
+        <p style="color: #2ca7eec2"> <strong>Contact</strong> </p>
+        <p>You can contact us at konlaptechs@gmail.com</p>
+        <div style="font-family: verdana; padding-top: 60px"> 
+            Go to <a href="https://konlap.co"> homepage </a> 
         </div>
-      </div>
 
     </div>
   );
 }
-
-
